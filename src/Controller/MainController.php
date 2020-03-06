7bd31fbc6e6ab7a76263a92106e73a27bb749c3b
Esmail Shabayek <esmail.shabayek@gmail.com>
2020-03-06 20:22:40 +0200

    Added new route with params

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,18 +3,45 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * Welcome Page
+     *
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller!',
+        //     'path' => 'src/Controller/MainController.php',
+        // ]);
+        return new Response('<h1>Welcome page</h1>');
+    }
+    /**
+     * About Page
+     *
+     * @Route("/about", name="about")
+     */
+    public function about()
+    {
+        return new Response('<h1>About page</h1>');
+    }
+    /**
+     * Posts Page
+     *
+     * @Route("/posts/{slug?}", name="posts")
+     * @param Request $request
+     * @return void
+     */
+    public function posts(Request $request)
+    {
+        $post = $request->get('slug');
+
+        return new Response('<h1>Posts page - ' . $post .'</h1>');
     }
 }
